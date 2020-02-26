b159b778930d2d48aae72cfed372782e5f9d14d1
BMEngineeR <cyz931123@gmail.com>
2020-02-26 16:19:16 -0500

    update for create object. setting threshhold for

--- R/CreateObject.R
+++ R/CreateObject.R
@@ -1,8 +1,10 @@
-CreateLTMGObject <- function(x = input_dir) {
-  
-  BRIC_Object<- new(Class = 'LTMG', InputData = as.matrix(x))
-  return(BRIC_Object)
-  
+CreateLTMGObject <- function(x = input_dir,min.cell = 0, min.gene = 0) {
+  raw.matrix <- as.matrix(x)
+  raw.matrix.filterbycell <- raw.matrix[(rowSums(raw.matrix > 0) > min.cell),]
+  raw.matrix.filterbygene <- raw.matrix.filterbycell[(colSums(raw.matrix.filterbycell > 0) > min.gene),]
+  LTMG_Object<- new(Class = 'LTMG', InputData = as.matrix(raw.matrix.filterbygene))
+  return(LTMG_Object)
 }
+
 
 
