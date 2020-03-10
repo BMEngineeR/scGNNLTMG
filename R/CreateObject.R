d732005a7e07f2ee266f0e399365e365ca33998c
BMEngineeR <cyz931123@gmail.com>
2020-03-10 15:29:43 -0400

    update bug input filtering method

--- R/CreateObject.R
+++ R/CreateObject.R
@@ -1,8 +1,12 @@
 CreateLTMGObject <- function(x = input_dir,min.cell = 0, min.gene = 0) {
   raw.matrix <- as.matrix(x)
   raw.matrix.filterbycell <- raw.matrix[(rowSums(raw.matrix > 0) > min.cell),]
-  raw.matrix.filterbygene <- raw.matrix.filterbycell[(colSums(raw.matrix.filterbycell > 0) > min.gene),]
-  LTMG_Object<- new(Class = 'LTMG', InputData = as.matrix(raw.matrix.filterbygene))
+  raw.matrix.filterbygene <- raw.matrix.filterbycell[,(colSums(raw.matrix.filterbycell > 0) > min.gene)]
+  message("Creating BRIC object. \n",
+          "The original input file contains ", dim(raw.matrix)[2], " cells and ", dim(raw.matrix)[1], " genes \n",
+          "Removed ", dim(raw.matrix)[1] - dim(raw.matrix.filterbycell)[1], " genes that total expression value is equal or less than ", min.cell, "\n",
+          "Removed ", dim(raw.matrix.filterbycell)[2] - dim(raw.matrix.filterbygene)[2], " cells that number of expressed gene is equal or less than ", min.gene
+  )
   return(LTMG_Object)
 }
 
