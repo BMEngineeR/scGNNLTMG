ca0346d52f6e14cdad3513aa448437fa0beaa231
BMEngineeR <cyz931123@gmail.com>
2020-03-31 13:02:45 -0400

    add logic, user control gene name and cell name

--- R/InputData.R
+++ R/InputData.R
@@ -2,20 +2,26 @@
 #' Title
 #'
 #' @param object
+#' @param path output path
+#' @param gene.name whether output gene name for sparse matrix
+#' @param cell.name whether output cell name for sparse matrix
 #'
 #' @return
 #' @name WriteSparse
 #' @examples
 #' @importFrom  Matrix Matrix writeMM
-writesparse <- function(object = NULL, path = "./") {
+writesparse <- function(object = NULL, path = "./", gene.name = FALSE, cell.name = FALSE) {
   if(!require("Matrix")){
     install.packages("Matrix")
   }
   my.sparse <- Matrix(object@OrdinalMatrix,sparse = T)
   writeMM(my.sparse,file = paste0(path,"/LTMG_sparse.mtx"))
-  write.table(data.frame(Barcode =colnames(my.sparse)) ,file= paste0(path,"barcode.txt"),sep = "\t",quote = F,row.names = F)
-  write.table(data.frame(Gene = rownames(my.sparse)),file = paste0(path,"Gene.txt"),sep = "\t",quote = F,row.names = F)
-
+  if(cell.name == TRUE){
+    write.table(data.frame(Barcode =colnames(my.sparse)) ,file= paste0(path,"barcode.txt"),sep = "\t",quote = F,row.names = F)
+  }
+  if(gene.name == TRUE){
+    write.table(data.frame(Gene = rownames(my.sparse)),file = paste0(path,"Gene.txt"),sep = "\t",quote = F,row.names = F)
+  }
 }
 
 #' @export
--- R/example.R
+++ R/example.R
@@ -1,7 +1,7 @@
 # devtools::install_github("BMEngineeR/scGNNLTMG")
-# setwd("d:/my_analysis/BRIC_TEST/2.Yan/")
+
 # x <- read.csv("d:/my_analysis/BRIC_TEST/2.Yan/Yan_expression.csv",header = T,row.names = 1,check.names = F)
 # library(scGNNLTMG)
 # object <- CreateLTMGObject(x)
 # object <-RunLTMG(object,Gene_use = 100)
-# WriteSparse(object,path = "d:/BMBL/")
+# WriteSparse(object,path = "d:/BMBL")
