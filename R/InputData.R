08f026eedb3776a9ac9cc3e1f043499fcf3fc91f
BMEngineeR <cyz931123@gmail.com>
2020-04-23 22:21:58 -0400

    update if sparese out all equal to one, write another sparse  matrix

--- R/InputData.R
+++ R/InputData.R
@@ -1,24 +1,25 @@
+#' Title
+#'
+#' @param x
+#' @param file
+#' @export
+#' @return
+#'
+#' @examples
 writeMM_new <- function(x, file) {
   mtype <- "real"
   if (is(x, "ngCMatrix")) {
     mtype <- "integer"
   }
-  writeLines(
-    c(
-      sprintf("%%%%MatrixMarket matrix coordinate %s general", mtype),
-      sprintf("%s %s %s", x@Dim[1], x@Dim[2], length(x@x))
-    ),
-    file
-
-  )
-  data.table::fwrite(
-    x = summary(x),
-    file = file,
-    append = TRUE,
-    sep = " ",
-    row.names = FALSE,
-    col.names = FALSE
-  )
+  writeLines(c(
+    sprintf("%%%%MatrixMarket matrix coordinate %s general", mtype),
+    sprintf("%s %s %s", x@Dim[1], x@Dim[2], length(x@x))
+    ),file)
+  i = x@i+1
+  j = findInterval(seq(x@x)-1,x@p[-1])+1
+  value = x@x
+  my.summary <- data.frame(i = i, j = j, value = value)
+  write.table(my.summary,file =file,sep = " ",row.names = F,append = T,col.names = F)
 }
 #' Title
 #'
@@ -31,13 +32,13 @@ writeMM_new <- function(x, file) {
 #' @name WriteSparse
 #' @examples
 #' @importFrom  Matrix Matrix writeMM
-writesparse <- function(object = NULL, path = "./", gene.name = TRUE, cell.name = TRUE) {
+writesparse <- function(object = NULL, path = getwd(), gene.name = TRUE, cell.name = TRUE) {
   if(!require("Matrix")){
     install.packages("Matrix")
   }
   my.sparse <- Matrix(object@OrdinalMatrix,sparse = T)
   if(all(my.sparse <=1)){
-    writeMM_new(my.sparse,file = "LTMG_sparse.mtx")
+    writeMM_new(x=my.sparse,file = paste0(path,"/LTMG_sparse.mtx"))
   }else{
     writeMM(my.sparse,file = paste0(path,"/LTMG_sparse.mtx"))
   }
